07e051cbd956b1107494e8efe3066d6efb7699d6
usuiat <stripewani@gmail.com>
2021-11-18 22:42:22 +0900

    VideoViewを追加。
    Accompanist-pagerのバージョンも更新。currentPageが0になる問題が修正されていた。

--- app/src/main/java/net/engawapg/app/viewonlyviewer/ViewerComposables.kt
+++ app/src/main/java/net/engawapg/app/viewonlyviewer/ViewerComposables.kt
@@ -1,5 +1,7 @@
 package net.engawapg.app.viewonlyviewer
 
+import android.media.MediaPlayer
+import android.widget.VideoView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.google.accompanist.pager.ExperimentalPagerApi
@@ -21,15 +24,19 @@ import com.google.accompanist.pager.rememberPagerState
 @Composable
 fun ViewerScreen(viewModel: MainViewModel, index: Int) {
     val items: List<GalleryItem> by viewModel.galleryItems.observeAsState(listOf())
-    Surface(color = Color.Black) {
-        val pagerState = rememberPagerState(initialPage = index)
+    if (items.isNotEmpty()) {
+        Surface(color = Color.Black) {
+            val pagerState = rememberPagerState(initialPage = index)
 
-        HorizontalPager(count = items.size, state = pagerState, itemSpacing = 10.dp) { pageIndex ->
-            Box {
-                var videoPlaying by remember { mutableStateOf(false) }
-                if (!videoPlaying) {
-                    Viewer(items[pageIndex])
-                }
+            HorizontalPager(
+                count = items.size,
+                state = pagerState,
+                itemSpacing = 10.dp
+            ) { pageIndex ->
+                Viewer(
+                    item = items[pageIndex],
+                    isCurrentPage = (pagerState.currentPage == pageIndex)
+                )
             }
         }
     }
@@ -37,11 +44,50 @@ fun ViewerScreen(viewModel: MainViewModel, index: Int) {
 
 @ExperimentalCoilApi
 @Composable
-fun Viewer(item: GalleryItem) {
+fun Viewer(item: GalleryItem, isCurrentPage: Boolean) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        var videoPlaying by remember { mutableStateOf(false) }
+        if (item.isVideo && isCurrentPage) {
+            VideoPlayer(item = item) {
+                videoPlaying = true
+            }
+        } else {
+            videoPlaying = false
+        }
+        if (!videoPlaying) {
+            ImageViewer(item)
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+fun ImageViewer(item: GalleryItem) {
     Image(
         painter = rememberImagePainter(data = item.uri),
         contentDescription = "Image",
         modifier = Modifier.fillMaxSize(),
         contentScale = ContentScale.Fit
+    )
+}
+
+@Composable
+fun VideoPlayer(item: GalleryItem, onStart: ()->Unit) {
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            VideoView(context).apply {
+                setVideoURI(item.uri)
+                setOnPreparedListener {
+                    start()
+                }
+                setOnInfoListener { _, what, _ ->
+                    if (what == MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START) {
+                        onStart()
+                    }
+                    false
+                }
+            }
+        }
     )
 }
