83fe8afbca4b12a25477938eec00b004e53dba12
usuiat <stripewani@gmail.com>
2022-04-02 00:02:52 +0900

    Use accompanist-permissions to require permission.

--- app/src/main/java/net/engawapg/app/viewonlyviewer/MainActivity.kt
+++ app/src/main/java/net/engawapg/app/viewonlyviewer/MainActivity.kt
@@ -4,12 +4,12 @@ import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -17,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.core.content.PermissionChecker
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -28,21 +27,18 @@ import coil.ImageLoader
 import coil.decode.VideoFrameDecoder
 import coil.fetch.VideoFrameFileFetcher
 import coil.fetch.VideoFrameUriFetcher
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import net.engawapg.app.viewonlyviewer.ui.theme.ViewOnlyViewerTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
 
-    companion object {
-        const val REQ_PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE
-    }
-
     private val viewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        checkPermission()
 
         val imageLoader = ImageLoader.Builder(this)
             .componentRegistry {
@@ -56,18 +52,8 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ViewOnlyViewerTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    AppScreen(viewModel = viewModel) { rationaleResult ->
-                        if (rationaleResult) {
-                            /* Permissionが必要な理由を説明し、了承されたので再度Permission要求 */
-                            viewModel.permissionState.value = PermissionState.UNKNOWN
-                            permissionRequest.launch(REQ_PERMISSION)
-                        } else {
-                            /* Permissionが必要な理由を説明したが、拒否された */
-                            viewModel.permissionState.value = PermissionState.DENIED
-                        }
-                    }
+                    AppScreen(viewModel = viewModel)
                 }
             }
         }
@@ -75,45 +61,18 @@ class MainActivity : ComponentActivity() {
 
     override fun onResume() {
         super.onResume()
-        viewModel.loadGallery()
-    }
-
-    /* Permissionの結果を受け取る */
-    private val permissionRequest = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ){ granted ->
-        viewModel.permissionState.value = when {
-            /* 承認 */
-            granted -> PermissionState.GRANTED
-            /* 拒否(1回目) Permissionが必要な理由を説明 */
-            shouldShowRequestPermissionRationale(REQ_PERMISSION) -> PermissionState.EXPLAINING
-            /* 拒否(2回目以降) */
-            else -> PermissionState.DENIED
-        }
-    }
-
-    /* 必要なPermissionが与えられているかどうかを確認 */
-    private fun checkPermission() {
-        val result = PermissionChecker.checkSelfPermission(this, REQ_PERMISSION)
-        if (result == PermissionChecker.PERMISSION_GRANTED) {
-            /* 承認済み */
-            viewModel.permissionState.value = PermissionState.GRANTED
-        } else {
-            /* 要求する */
-            permissionRequest.launch(REQ_PERMISSION)
-        }
+//        viewModel.loadGallery() // TODO
     }
 }
 
 @Composable
-fun AppScreen(viewModel: MainViewModel, onRationaleDialogResult: (Boolean) -> Unit) {
+fun AppScreen(viewModel: MainViewModel) {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "gallery") {
         composable("gallery") {
             GalleryScreen(
                 viewModel = viewModel,
-                onItemSelected = { index -> navController.navigate("viewer/$index") },
-                onRationaleDialogResult = onRationaleDialogResult
+                onItemSelected = { index -> navController.navigate("viewer/$index") }
             )
         }
         composable(
@@ -126,11 +85,11 @@ fun AppScreen(viewModel: MainViewModel, onRationaleDialogResult: (Boolean) -> Un
     }
 }
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun GalleryScreen(
     viewModel: MainViewModel,
-    onItemSelected: (Int)->Unit = {},
-    onRationaleDialogResult: (Boolean)->Unit
+    onItemSelected: (Int)->Unit = {}
 ) {
     val systemUiController = rememberSystemUiController()
     val statusBarColor = MaterialTheme.colorScheme.primaryContainer
@@ -139,16 +98,25 @@ fun GalleryScreen(
     }
 
     /* Permissionの取得状況によって表示内容を変える */
-    val permissionState: PermissionState by viewModel.permissionState.observeAsState(PermissionState.UNKNOWN)
-    val items: List<GalleryItem> by viewModel.galleryItems.observeAsState(listOf())
-    when (permissionState) {
-        /* 承認: ギャラリー表示 */
-        PermissionState.GRANTED -> Gallery(items, onItemSelected)
-        /* 拒否: 設定で許可するように説明 */
-        PermissionState.DENIED -> AskPermissionInSettingApp()
-        /* 拒否（1回目）: Permissionが必要な理由をダイアログで説明 */
-        PermissionState.EXPLAINING -> PermissionRationaleDialog(onRationaleDialogResult)
-        else -> {}
+    val ps = rememberPermissionState(Manifest.permission.READ_EXTERNAL_STORAGE)
+    when {
+        ps.hasPermission -> {
+            val items: List<GalleryItem> by viewModel.galleryItems.observeAsState(listOf())
+            LaunchedEffect(ps) {
+                viewModel.loadGallery()
+            }
+            Gallery(items, onItemSelected)
+        }
+
+        ps.shouldShowRationale -> PermissionRationaleDialog {
+            ps.launchPermissionRequest()
+        }
+
+        ps.permissionRequested -> AskPermissionInSettingApp()
+
+        else -> SideEffect {
+            ps.launchPermissionRequest()
+        }
     }
 }
 
@@ -166,21 +134,13 @@ fun AskPermissionInSettingApp() {
 }
 
 @Composable
-fun PermissionRationaleDialog(onDialogResult: (Boolean)->Unit) {
+fun PermissionRationaleDialog(onDialogResult: ()->Unit) {
     AlertDialog(
         text = { Text(stringResource(R.string.rationale_permission)) },
         onDismissRequest = {},
         confirmButton = {
-            TextButton(onClick = {
-                onDialogResult(true)
-            }) {
+            TextButton(onClick = onDialogResult) {
                 Text(stringResource(R.string.ok))
-            }},
-        dismissButton = {
-            TextButton(onClick = {
-                onDialogResult(false)
-            }) {
-                Text(stringResource(R.string.cancel))
             }},
     )
 }
--- app/src/main/java/net/engawapg/app/viewonlyviewer/MainViewModel.kt
+++ app/src/main/java/net/engawapg/app/viewonlyviewer/MainViewModel.kt
@@ -3,27 +3,16 @@ package net.engawapg.app.viewonlyviewer
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-
-enum class PermissionState {
-    UNKNOWN,
-    GRANTED,    /* 承認された */
-    DENIED,     /* 拒否された */
-    EXPLAINING  /* 追加説明が必要 */
-}
+import kotlinx.coroutines.withContext
 
 class MainViewModel(private val model: GalleryModel, app: Application): AndroidViewModel(app) {
-    val permissionState = MutableLiveData<PermissionState>().apply { value = PermissionState.UNKNOWN }
     val galleryItems = MutableLiveData<List<GalleryItem>>().apply { value = listOf() }
 
-    fun loadGallery() {
-        viewModelScope.launch(Dispatchers.IO) {
-            if (permissionState.value == PermissionState.GRANTED) {
-                model.load(getApplication())
-                galleryItems.postValue(model.items)
-            }
+    suspend fun loadGallery() {
+        withContext(Dispatchers.IO) {
+            model.load(getApplication())
         }
+        galleryItems.value = model.items
     }
 }
